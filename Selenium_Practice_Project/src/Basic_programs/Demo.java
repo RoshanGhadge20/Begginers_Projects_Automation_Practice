cdbf49bb52e9f7b3a1d50d8768ddaab923fa26a7
Roshan Ghadge <roshanghadge20@gmail.com>
2023-12-17 18:56:54 +0530

    This is the small change that we made in basic program code

--- Selenium_Practice_Project/src/Basic_programs/Demo.java
+++ Selenium_Practice_Project/src/Basic_programs/Demo.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/Admin/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://testautomationpractice.blogspot.com/");
 		Thread.sleep(3000);
 		driver.quit();
+
+		System.out.println("End of Program Execution");
 	}
 
 }
